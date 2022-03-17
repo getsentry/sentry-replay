f64bbd0a45ad5a76b2b8890fc138002ebfcd3f33
Billy Vong <billy@sentry.io>
2022-03-17 15:02:57 -0400

    temp demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,12 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as Sentry from '@sentry/browser';
 import { SentryReplay } from '@sentry/replay';
+import { BrowserTracing, } from "@sentry/tracing";
 
 Sentry.init({
+  // temp: sentry-test/billy-test
+  debug: true,
   dsn:
-    'https://375821616abb4d8c94f43726ed08e27f@o19635.ingest.sentry.io/2273529',
+    'https://24f526f0cefc4083b2546207a3f6811d@o19635.ingest.sentry.io/5415672',
   environment: 'demo',
-  integrations: [new SentryReplay()],
+  tracesSampleRate: 1.0,
+  integrations: [
+    new SentryReplay({idleTimeout: 10000}),
+    new BrowserTracing({
+      tracingOrigins: ["localhost:3000", "localhost", /^\//],
+    }),
+  ],
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
