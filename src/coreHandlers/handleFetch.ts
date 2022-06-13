ebabe5a515bcda0775b519ae4cfa699c47696493
Billy Vong <billy@sentry.io>
2022-06-13 16:37:20 -0400

    cleanup

--- src/coreHandlers/handleFetch.ts
+++ src/coreHandlers/handleFetch.ts
@@ -1,13 +1,16 @@
 import { ReplaySpan } from '@/types';
+import { getCurrentHub } from '@sentry/browser';
 
 export function handleFetch(handlerData: any): ReplaySpan {
   if (!handlerData.endTimestamp) {
     return null;
   }
 
+  const [op, description] = handlerData.args;
+
   return {
-    description: handlerData.args[1],
-    op: handlerData.args[0],
+    description,
+    op,
     startTimestamp: handlerData.startTimestamp / 1000,
     endTimestamp: handlerData.endTimestamp / 1000,
     data: {
