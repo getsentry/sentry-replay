4c360275fae6bba0c68735348c505fa256b0ec48
Billy Vong <billyvg@users.noreply.github.com>
2022-08-05 11:39:29 -0400

    feat: use core SDK to handle fetch/xhr requests
    Instead of using `performance`, use core SDK instrumentation as it provides a bit more detail (e.g. status codes and method).
    
    Partially addresses #137

--- src/coreHandlers/handleXhr.ts
+++ src/coreHandlers/handleXhr.ts
@@ -1,7 +1,40 @@
-import { ReplaySpan } from '@/types';
+import { ReplayPerformanceEntry } from '@/createPerformanceEntry';
 
-export function handleXhr(handlerData: any): ReplaySpan {
+// From sentry-javascript
+type XHRSendInput =
+  | null
+  | Blob
+  | BufferSource
+  | FormData
+  | URLSearchParams
+  | string;
+interface SentryWrappedXMLHttpRequest extends XMLHttpRequest {
+  [key: string]: any;
+  __sentry_xhr__?: {
+    method?: string;
+    url?: string;
+    status_code?: number;
+    body?: XHRSendInput;
+    startTimestamp?: number; // This is unique to replay SDK
+  };
+}
+
+interface XhrHandlerData {
+  args: [method: string, url: string];
+  xhr: SentryWrappedXMLHttpRequest;
+  startTimestamp: number;
+  endTimestamp?: number;
+}
+
+export function handleXhr(handlerData: XhrHandlerData): ReplayPerformanceEntry {
+  if (handlerData.xhr.__sentry_own_request__) {
+    // Taken from sentry-javascript
+    // Only capture non-sentry requests
+    return;
+  }
+
   if (handlerData.startTimestamp) {
+    // TODO: See if this is still needed
     handlerData.xhr.__sentry_xhr__.startTimestamp = handlerData.startTimestamp;
   }
 
@@ -9,17 +42,22 @@ export function handleXhr(handlerData: any): ReplaySpan {
     return null;
   }
 
-  const [op, description] = handlerData.args;
+  const {
+    method,
+    url,
+    status_code: statusCode,
+  } = handlerData.xhr.__sentry_xhr__ || {};
 
   return {
-    description,
-    op,
-    startTimestamp:
+    type: 'resource.xhr',
+    name: url,
+    start:
       handlerData.xhr.__sentry_xhr__.startTimestamp / 1000 ||
       handlerData.endTimestamp / 1000.0,
-    endTimestamp: handlerData.endTimestamp / 1000.0,
+    end: handlerData.endTimestamp / 1000.0,
     data: {
-      statusCode: handlerData.response.status,
+      method,
+      statusCode,
     },
   };
 }
